868541aef8ac1cd92cea426f9d621c6a3e266a7e
DESKTOP-HF2S5R4\jiami <jiamingy@usc.edu>
2017-09-10 12:42:09 -0700

    Download standard assets for this to work

---
+++ Intermediate/Assets/Flicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.value >= 0.3)
+        {
+            if(GetComponent<Light>().intensity == 2)
+            {
+                GetComponent<Light>().intensity = 1;
+            }
+            else
+            {
+                GetComponent<Light>().intensity = 2;
+            }
+        }
+	}
+
+    IEnumerator wait ()
+    {
+        yield return new WaitForSeconds(5);
+    }
+}
